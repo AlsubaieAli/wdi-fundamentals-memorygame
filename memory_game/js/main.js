733321aa076953ec5612ba0f177d0d4809f39aaa
abalsuba <abalsuba@asu.edu>
2018-09-26 22:47:00 +0300

    creaed game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,23 @@
 var cards = ['queen', 'queen', 'king', 'king'],
     cardsInPlay = [];
 
-var cardOne = cards[0], cardTwo = cards[2];
+function checkForMatch(){
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if(cardsInPlay.length === 2){
-    if(cardsInPlay[0] === cardsInPlay[1]){
-        alert('You found a match!');
-    }
-    else{
-        alert('Sorry, try again.');
+    if(cardsInPlay.length === 2){
+        if(cardsInPlay[0] === cardsInPlay[1]){
+            alert('You found a match!');
+        }else{
+            alert('Sorry, try again.');
+        }
     }
 }
+
+function flipCard(cardID){
+    var card = cards[cardID];
+    cardsInPlay.push(card);
+    console.log("User flipped " + card);
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
